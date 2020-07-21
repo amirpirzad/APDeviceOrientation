3667821db7df9f0d1c07e7fb2f7994d2c085f3c6
amir pirzad <pirzad7@gmail.com>
2020-07-21 16:44:42 +0430

    Initial Commit

---
+++ APDeviceOrientation/APDeviceOrientation.h
@@ -0,0 +1,19 @@
+//
+//  APDeviceOrientation.h
+//  APDeviceOrientation
+//
+//  Created by pirzad on 7/21/20.
+//  Copyright © 2020 pirzad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APDeviceOrientation.
+FOUNDATION_EXPORT double APDeviceOrientationVersionNumber;
+
+//! Project version string for APDeviceOrientation.
+FOUNDATION_EXPORT const unsigned char APDeviceOrientationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APDeviceOrientation/PublicHeader.h>
+
+
